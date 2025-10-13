a0d1e911ad6dbee18ac64269d5f92d6d01da42f3
Evgeniy Kungurov <johnk2280@gmail.com>
2025-10-13 16:58:59 +0500

    [upd]

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+fn scope_test_1() {
+    let some_var = {
+        let s = "Hello world";
+        s
+    };
+    println!("Значение переменной some_var: {}", some_var);
+    // println!("Значение переменной внутренней области видимости s: {}", s);  // буде ошибка,
+    // т.к. s находится в другой (вложенной) области видимости
+}
+
+fn string_test_1() {
+    let mut s = String::from("Hello");
+    println!("До: {}", s);
+    s.push_str(", world");
+    println!("После: {}", s)
+}
+
 fn main() {
-    println!("Hello, world!");
+    scope_test_1();
+    string_test_1();
 }
