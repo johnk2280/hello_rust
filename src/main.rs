33c5a2b91070ade469653876889c83042af8134b
Evgeniy Kungurov <johnk2280@gmail.com>
2025-10-22 06:49:23 +0500

    [upd]

--- src/main.rs
+++ src/main.rs
@@ -204,29 +204,43 @@
 //     println!("{}", some_number + concrete_number);
 // }
 
-#[derive(Debug)]
-enum UseState {
-    Alabama,
-    Alaska,
-}
+// #[derive(Debug)]
+// enum UseState {
+//     Alabama,
+//     Alaska,
+// }
+//
+// enum Coin {
+//     Penny,
+//     Nickel,
+//     Dime,
+//     Quarter(UseState),
+// }
+//
+// fn pattern_matching_test_1(coin: Coin) -> i8 {
+//     match coin {
+//         Coin::Penny => 1,
+//         Coin::Nickel => 5,
+//         Coin::Dime => 10,
+//         Coin::Quarter(state) => {
+//             println!("State: {:?}", state);
+//             25
+//         }
+//     }
+// }
 
-enum Coin {
-    Penny,
-    Nickel,
-    Dime,
-    Quarter(UseState),
-}
-
-fn pattern_matching_test_1(coin: Coin) -> i8 {
-    match coin {
-        Coin::Penny => 1,
-        Coin::Nickel => 5,
-        Coin::Dime => 10,
-        Coin::Quarter(state) => {
-            println!("State: {:?}", state);
-            25
-        }
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
     }
+}
+
+fn test_option_2() {
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
+    println!("{:?}, {:?}, {:?}", five, six, none);
 }
 
 fn main() {
@@ -250,6 +264,7 @@ fn main() {
     // enum_test_3();
 
     // test_option_1();
-    let coin_ = pattern_matching_test_1(Coin::Quarter(UseState::Alabama));
-    println!("{}", coin_);
+    // let coin_ = pattern_matching_test_1(Coin::Quarter(UseState::Alabama));
+    // println!("{}", coin_);
+    test_option_2();
 }
