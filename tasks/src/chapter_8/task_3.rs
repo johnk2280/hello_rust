a26957399473ab9d6aefadef7221bc31b75bbb94
Evgeniy Kungurov <johnk2280@gmail.com>
2025-10-31 20:31:00 +0500

    [add] task_3.rs

---
+++ tasks/src/chapter_8/task_3.rs
@@ -0,0 +1,10 @@
+use std::collections::HashMap;
+
+pub mod company {
+
+    let mut employees = HashMap::new();
+
+    pub fn add_to_department(department: String, employee: String) {
+        employees.insert();
+    }
+}
